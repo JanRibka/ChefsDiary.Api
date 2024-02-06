4be87c511a0e29507da4113ec68772e617ab272a
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-02-06 16:34:49 +0100

    Oprava migrací

--- migrations/Version20240206153030.php
+++ migrations/Version20240206153030.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20240204181629 extends AbstractMigration
+final class Version20240206153030 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,12 @@ final class Version20240204181629 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE Users (IdUser INT UNSIGNED AUTO_INCREMENT NOT NULL, Login VARCHAR(50) NOT NULL, Password VARCHAR(255) NOT NULL, PRIMARY KEY(IdUser)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE Users DROP Name, DROP CreatedAt, DROP UpdatedAt, CHANGE Email Login VARCHAR(50) NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE Users');
+        $this->addSql('ALTER TABLE Users ADD Name VARCHAR(50) DEFAULT NULL, ADD CreatedAt DATETIME DEFAULT NULL, ADD UpdatedAt DATETIME DEFAULT NULL, CHANGE Login Email VARCHAR(50) NOT NULL');
     }
 }
