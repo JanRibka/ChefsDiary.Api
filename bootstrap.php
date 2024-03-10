a25ccc7e2e3945f3eb5bed6a914e0c193fa1718a
JanRibka <62721322+JanRibka@users.noreply.github.com>
2024-03-10 21:44:21 +0100

    Allowed origins

--- bootstrap.php
+++ bootstrap.php
@@ -10,4 +10,6 @@ require __DIR__ . '/configs/path_constants.php';
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
+require CONFIG_PATH . '/allowed_origins.php';
+
 return require CONFIG_PATH . '/container/container.php';
